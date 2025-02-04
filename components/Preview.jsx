9b3185fb1a3559eb512b7ba233b8bcc1e5b1e307
Jahaziel <jahzerahj@gmail.com>
2025-02-04 16:39:17 +0100

    added favorite page functionality

--- components/Preview.jsx
+++ components/Preview.jsx
@@ -2,10 +2,10 @@ import Image from "next/image";
 
 export default function Preview({ artist, pieceName, image }) {
   return (
-    <li>
+    <>
       <Image src={image} height={80} width={80} alt="Alt" />
       <p>Title: {pieceName}</p>
       <p>Artist: {artist}</p>
-    </li>
+    </>
   );
 }
