12fa5a34755f2c8024604070ab4fbfadce25752c
Idan Benik <129896903+idn8k@users.noreply.github.com>
2025-02-05 13:51:59 +0100

    styled fav & details pages

--- pages/art-pieces/[slug].js
+++ pages/art-pieces/[slug].js
@@ -2,6 +2,28 @@ import Link from "next/link";
 import ArtDetails from "@/components/ArtDetail";
 import { useRouter } from "next/router";
 import Heart from "@/components/Heart";
+import styled from "styled-components";
+import styles from "@/styles";
+
+const StyledDetailsContainer = styled.div`
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 10px auto;
+  height: auto;
+  border-radius: 40px;
+  overflow: hidden;
+  position: relative;
+`;
+
+const StyledBtn = styled.button`
+  position: absolute;
+  background: transparent;
+  border: none;
+  right: 50px;
+  top: 40px;
+`;
 
 export default function Details({
   data,
@@ -22,38 +44,18 @@ export default function Details({
   return (
     <>
       <Link href="/art-pieces">BACK</Link>
-      <br></br>
-      <ArtDetails
-        image={currDetails.imageSource}
-        currDetailsName={currDetails.name}
-        artist={currDetails.artist}
-        year={currDetails.year}
-        genre={currDetails.genre}
-      />
-      <button onClick={() => toggleFavPieces(currDetails.slug)}>
-        <Heart fav={currDetails.slug} favPieces={favPieces} />
-      </button>
+      <StyledDetailsContainer>
+        <ArtDetails
+          image={currDetails.imageSource}
+          currDetailsName={currDetails.name}
+          artist={currDetails.artist}
+          year={currDetails.year}
+          genre={currDetails.genre}
+        />
+        <StyledBtn onClick={() => toggleFavPieces(currDetails.slug)}>
+          <Heart fav={currDetails.slug} favPieces={favPieces} />
+        </StyledBtn>
+      </StyledDetailsContainer>
     </>
   );
 }
-
-// {
-//   "slug": "orange-red-and-green",
-//   "artist": "Steve Johnson",
-//   "name": "Orange Red and Green Abstract Painting",
-//   "imageSource": "https://example-apis.vercel.app/assets/art/orange-red-and-green.jpg",
-//   "year": "2018",
-//   "genre": "Abstract Painting",
-//   "colors": [
-//     "#0f5855",
-//     "#e6ba15",
-//     "#b42011",
-//     "#cec4c6",
-//     "#d5561f"
-//   ],
-//   "dimensions": {
-//     "height": 2432,
-//     "width": 1920,
-//     "type": "jpg"
-//   }
-// },
--- pages/favorites.js
+++ pages/favorites.js
@@ -2,28 +2,50 @@ import Preview from "@/components/Preview";
 import { Fragment } from "react";
 import Heart from "@/components/Heart";
 
+import styled from "styled-components";
+import styles from "@/styles";
+
+const StyledPreviewsContainer = styled.div`
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 10px auto;
+  height: auto;
+  border-radius: 40px;
+  overflow: hidden;
+  position: relative;
+`;
+
+const StyledBtn = styled.button`
+  position: absolute;
+  background: transparent;
+  border: none;
+  right: 50px;
+  top: 40px;
+`;
+
 export default function Favorites({ favPieces, data, toggleFavPieces }) {
   console.log("data:", data);
 
   return (
-    <Fragment>
+    <>
       {data.map((piece) =>
         favPieces.includes(piece.slug) ? (
-          <>
+          <StyledPreviewsContainer key={piece.slug}>
             <Preview
-              key={piece.slug}
               artist={piece.artist}
               pieceName={piece.name}
               image={piece.imageSource}
             />
-            <button onClick={() => toggleFavPieces(piece.slug)}>
+            <StyledBtn onClick={() => toggleFavPieces(piece.slug)}>
               <Heart fav={piece.slug} favPieces={favPieces} />
-            </button>
-          </>
+            </StyledBtn>
+          </StyledPreviewsContainer>
         ) : (
           ""
         )
       )}
-    </Fragment>
+    </>
   );
 }
